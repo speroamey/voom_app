5147fa5a5b28dfe569939f88406d8c84a7624e0f
Spero AMEY <ameyspero@gmail.com>
2018-05-28 20:00:22 +0100

    design login page

--- lib/login.dart
+++ lib/login.dart
@@ -31,22 +31,31 @@ class _LoginState extends State<Login> {
       tag: "log",
       child: CircleAvatar(
         backgroundColor: Colors.transparent,
-        radius: 48.0,
-        child: Image.asset("images/voiture.jpg"),
+        radius: 50.0,
+        child: new Text("Connexion")
       ),
     );
 
-    final phoneNumber = new TextFormField(
-      controller: phoneCtrl,
-      keyboardType: TextInputType.phone,
-      autofocus: false,
-      onFieldSubmitted: (String value) {
-        _onLogin();
-      },
-      decoration: InputDecoration(
-        hintText: "Numéro de téléphone",
-        contentPadding: EdgeInsets.fromLTRB(20.0, 11.0, 20.0, 11.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+    final phoneNumber = new Theme(
+      data: new ThemeData(primaryColor:  Colors.white30,
+        hintColor: Colors.white30
+      ),
+      child: new TextFormField(
+        controller: phoneCtrl,
+        keyboardType: TextInputType.phone,
+        autofocus: true,
+        onFieldSubmitted: (String value) {
+          _onLogin();
+        },
+        decoration: InputDecoration(
+          
+          hintText: "Numéro de téléphone",
+          contentPadding: EdgeInsets.fromLTRB(20.0, 11.0, 20.0, 11.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(32.0),
+           
+            ),
+        ),
       ),
     );
 
@@ -60,25 +69,28 @@ class _LoginState extends State<Login> {
             elevation: 5.0,
             child: new FlatButton(
                 textColor: Colors.white,
-                disabledColor: Colors.grey.shade300,
-                disabledTextColor: Colors.black26,
+                disabledColor: Colors.white,
+                disabledTextColor: Colors.black38,
                 onPressed: phoneCtrl.text.trim().isEmpty ? null : _onLogin,
-                color: Colors.redAccent,
+                color: Colors.redAccent[100],
                 child: new Text("Validez"))));
 
     return new Scaffold(
-        backgroundColor: Colors.white,
-        body: new Center(
-            child: new ListView(
-                shrinkWrap: true,
-                padding: EdgeInsets.only(left: 24.0, right: 24.0),
-                children: <Widget>[
-              logo,
-              SizedBox(height: 48.0),
-              phoneNumber,
-              SizedBox(height: 8.0),
-              loginButton
-            ])));
+       /*  backgroundColor: Colors.white, */
+        body: new Container(
+                color: Colors.red,
+                  child: new Center(
+              child: new ListView(
+                  shrinkWrap: true,
+                  padding: EdgeInsets.only(left: 24.0, right: 24.0),
+                  children: <Widget>[
+                logo,
+                SizedBox(height: 48.0),
+                phoneNumber,
+                SizedBox(height: 8.0),
+                loginButton
+              ])),
+        ));
   }
 
   _onLogin() async {
